bc8ca0fa4de3a2ec30fb14dfec05ed4c86861219
Vaibhav1505vkv <vaibhav@vkvtech.com>
2026-08-16 01:17:54 +0530

    Add API Gateway with Eureka discovery routing

--- config-server/src/main/java/com/ecommerce/config_server/ConfigServerApplication.java
+++ config-server/src/main/java/com/ecommerce/config_server/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.config_server;
+package com.ecommerce.config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- config-server/src/test/java/com/ecommerce/config_server/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/ecommerce/config_server/ConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.config_server;
+package com.ecommerce.config_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
